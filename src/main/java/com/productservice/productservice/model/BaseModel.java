38e889cc62bc950eb6f4f7c3fc395f7adec69213
Sadhana Chauhan <sadhanachauhan@Sadhanas-MacBook-Air.local>
2024-07-16 19:57:28 +0530

    added hibernate orm details application.properties, integrated UUID generator and connected to mysql db

--- src/main/java/com/productservice/productservice/model/BaseModel.java
+++ src/main/java/com/productservice/productservice/model/BaseModel.java
@@ -1,10 +1,22 @@
 package com.productservice.productservice.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseModel{
-    private String id;
+    @Id
+    @GeneratedValue(generator = "uuidgenerator")
+    @GenericGenerator(name = "uuidgenerator", strategy = "uuid2")
+    @Column(name = "id", columnDefinition = "binary(16)", unique = true, nullable = false, updatable = false)
+    protected UUID id;
 }
--- src/main/java/com/productservice/productservice/model/Product.java
+++ src/main/java/com/productservice/productservice/model/Product.java
@@ -1,14 +1,18 @@
 package com.productservice.productservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel{
     private String title;
     private String description;
     private double price;
     private String image;
+    @ManyToOne
     private Category category;
 }
